4772063772e02051a94215714f0722cd0779f252
boskyle <elitebos2@gmail.com>
2020-06-02 19:17:18 -0400

    added a new practice file to learn lists

---
+++ lists/list1.py
@@ -0,0 +1,17 @@
+hsAge = [14,15,16,17,18]
+
+#lists can consift of different data types
+hsAge2 = [14,15,16,17,"eighteen"]
+
+
+# prints all items inside the list
+print(hsAge)
+print(hsAge2)
+
+# prints all individual items inside the list
+print (hsAge[0])
+print (hsAge2[4])
+
+# for loop to iterate through all the items of list => hsAge
+for x in hsAge:
+    print(x)
