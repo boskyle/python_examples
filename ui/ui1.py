9b889a48d84bacaf567dffc483887b31e6e70d7f
boskyle <elitebos2@gmail.com>
2020-06-09 12:59:50 -0400

    added ui examples with input() function

---
+++ ui/ui1.py
@@ -0,0 +1,5 @@
+myName = input("Please enter your name: ")
+myAge = input("..what about your age? ")
+
+print("my name is ",myName,"and my age is ",myAge,"years old!")
+
---
+++ ui/ui2.py
@@ -0,0 +1,10 @@
+myName = input("Please enter your name: ")
+
+
+
+#we can add a formatter to an input function to make it more interactive
+#i can add in the name given by the user to this input
+myAge = input("%s..what about your age? "%(myName))
+
+print("my name is",myName,"and my age is ",myAge,"years old!")
+
