8b3bb2f61d33353f361e03b54e7c80e721f64431
boskyle <elitebos2@gmail.com>
2020-06-09 13:19:51 -0400

    added more examples

---
+++ conditions/c1.py
@@ -0,0 +1,8 @@
+userInput = input("Enter \'1\' for Chicken\nEnter \'2\' for Salmon\nEnter \'3\' for Steak\n")
+
+if userInput == '1':
+    print("Chicken! \nGood choice.. clean protein source")
+elif userInput == '2':
+    print("Salmon! \nExcellent choice! good source of good fats and protein")
+elif userInput == '3':
+    print("Steak \nNice choice... best served medium rare")
---
+++ string/triple_quotes.py
@@ -0,0 +1,8 @@
+
+
+#to display a long message and span over multiple lines
+
+print('''Wadddup.
+I am the next line.
+Insert motivational quote here...
+    ''')
